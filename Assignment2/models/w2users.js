9fea1a3d7755d6a46ac7b9377a8861a7fccc7725
Victor Liu <vctl95@gmail.com>
2023-05-02 14:34:15 -0700

    Change schema types

---
+++ Assignment2/models/w2users.js
@@ -0,0 +1,27 @@
+// for mongoose schemas and models
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    // "username": String,
+    "username": {
+        type: String,
+        required: true,
+        unique: true
+    },
+    "password": String,
+    "type": String,
+    "todos": [
+        {
+            "name": String,
+            // "done": Boolean
+            "done": {
+                type: Boolean,
+                default: false
+            }
+        }
+    ]
+});
+
+const usersModel = mongoose.model('w1user', usersSchema)
+
+module.exports = usersModel;
